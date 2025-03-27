e1c506caa5f7f5df2b970f337e547c19f2360733
adambardzak <104588473+adambardzak@users.noreply.github.com>
2025-03-27 13:06:11 +0100

    microsoft clarity

--- src/components/analytics/MicrosoftClarity.tsx
+++ src/components/analytics/MicrosoftClarity.tsx
@@ -2,7 +2,7 @@
 
 import Script from "next/script";
 
-export default function MicrosoftClarity({ clarityId }: { clarityId: string }) {
+export default function MicrosoftClarity() {
   return (
     <Script
       id="microsoft-clarity"
@@ -10,12 +10,12 @@ export default function MicrosoftClarity({ clarityId }: { clarityId: string }) {
       dangerouslySetInnerHTML={{
         __html: `
           (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "${clarityId}");
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "qurcvpm1ml");
         `,
       }}
     />
   );
-} 
+}
